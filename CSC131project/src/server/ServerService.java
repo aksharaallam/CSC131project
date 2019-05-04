00d4167ddb7f57ad70076da4b48cb8f953355703
Akshara reddy <48264790+aksharayeruva@users.noreply.github.com>
2019-05-03 22:38:33 -0700

    Add files via upload

---
+++ CSC131project/src/server/ServerService.java
@@ -0,0 +1,17 @@
+package server;
+
+import notification.Notification;
+
+public class ServerService {
+
+	public Notification getNotification() {
+		return null;
+	}
+	
+	public boolean sendNotification() {
+		return true;
+	}
+	
+	
+	
+}
